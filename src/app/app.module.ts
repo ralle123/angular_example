5852590c7f46ebd645e9b542d0bfe9af918f21c9
Raul Samayoa <ralle@Rauls-MacBook-Pro.local>
2020-08-27 14:27:22 -0400

    finished routing pages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,17 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './shared/header/header.component';
 import { FooterComponent } from './shared/footer/footer.component';
 import { PortofolioComponent } from './pages/portofolio/portofolio.component';
+import { AboutComponent } from './pages/about/about.component';
+import { ItemComponent } from './pages/item/item.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     FooterComponent,
-    PortofolioComponent
+    PortofolioComponent,
+    AboutComponent,
+    ItemComponent
   ],
   imports: [
     BrowserModule,
